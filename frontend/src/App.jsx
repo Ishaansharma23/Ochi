e9a7180ea70b6efdc94414e6e58ab9719b1d63c8
Ishaan Sharma <sharmaishaan0009@gmail.com>
2025-07-12 20:07:49 +0530

    Setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className='w-full h-screen bg-zinc-900'>
+       
+      </div>
+    </>
+  )
+}
+
+export default App
