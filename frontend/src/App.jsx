5c2d7aa37d70d13239e7a8a3e48af7af29b4ebcc
Ishaan Sharma <sharmaishaan0009@gmail.com>
2025-07-16 00:15:41 +0530

    Added cards section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import About from './components/About'
 import Eyes from './components/Eyes'
 import Featured from './components/Featured'
 import Cards from './components/Cards'
+import Footer from './components/Footer'
 
 function App() {
   
@@ -21,6 +22,7 @@ function App() {
        <Eyes />
        <Featured />
        <Cards/>
+       <Footer />
       </div>
     </>
   )
--- frontend/src/components/Cards.jsx
+++ frontend/src/components/Cards.jsx
@@ -4,11 +4,20 @@ const Cards = () => {
   return (
     <div className='w-full h-screen flex items-center px-30 gap-5'>
         <div className="cardcontainer w-1/2 h-[50vh]">
-            <div className="card w-full rounded-xl h-full bg-[#004D43]"></div>
+            <div className="card relative  w-full rounded-xl h-full bg-[#004D43] flex justify-center items-center">
+            <img src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg"/>
+            <button className='absolute left-10 bottom-10 px-5 py-1 border-2 rounded-full'>&copy;2019</button>
+            </div>
         </div>
         <div className="cardcontainer w-1/2 h-[50vh] flex gap-5">
-            <div className="card w-1/2 h-full rounded-xl bg-[#192826]"></div>
-            <div className="card w-1/2 h-full rounded-xl bg-[#192826]"></div>
+            <div className="card relative w-1/2 h-full rounded-xl bg-[#192826]  flex justify-center items-center">
+            <img src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg"/>
+            <button className='absolute left-10 bottom-10 px-5 py-1 border-2 rounded-full'>&copy;2020</button>
+            </div>
+            <div className="card relative w-1/2 h-full rounded-xl bg-[#192826] flex justify-center items-center">
+            <img src="https://ochi.design/wp-content/uploads/2022/04/logo001.svg"/>
+            <button className='absolute left-10 bottom-10 px-5 py-1 border-2 rounded-full'>&copy;2021</button>
+            </div>
         </div>
     </div>
   )
